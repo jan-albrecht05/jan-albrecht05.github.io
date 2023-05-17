0320fec5bf9fe45e81857c58ad4c1c28da61075d
Jan <janalbrecht05@gmail.com>
2023-05-17 20:47:28 +0200

    cleaning up old bugs

--- content/schule/info/app.js
+++ content/schule/info/app.js
@@ -13,19 +13,24 @@ function start(){
             let timeLeft = endTime - new Date().getTime();
 
             if(timeLeft > 0){
-            let minutes = timeLeft / (1000 * 60);
-            minutes = Math.floor(minutes);
-            let seconds = (timeLeft / 1000) % 60;
-            seconds = Math.round(seconds);
-            seconds = ('0' + seconds).slice(-2);
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            minutes = minutes < 100 ? " " + minutes : minutes;
-            let text = minutes + ' : ' + seconds;
-            timer.innerHTML = text;
+                let minutes = timeLeft / (1000 * 60);
+                minutes = Math.floor(minutes);
+                let seconds = (timeLeft / 1000) % 60;
+                seconds = Math.round(seconds);
+                seconds = ('0' + seconds).slice(-2);
+                minutes = minutes < 10 ? "0" + minutes : minutes;
+                minutes = minutes < 100 ? " " + minutes : minutes;
+                if (seconds == 60){
+                    seconds = "00";
+                }
+                let text = minutes + ' : ' + seconds;
+                timer.innerHTML = text;
             } 
             else {
                 timer.innerHTML = 'Die Zeit ist abgelaufen!';
+                timer.style.marginTop = "30vh";
                 document.getElementById("h1").style.display = "none";
+                document.getElementById("stop").innerHTML = "zurück"
             }
         }, 1000);
         timerStarted = true;
