ffaf1b1dc15fd37eb11d4236c0a40b00b32d2763
Jan <78158642+jan-albrecht05@users.noreply.github.com>
2023-08-01 12:12:31 +0200

    deleting the dropdown from the navbar to add it via JS

--- js/navbar.js
+++ js/navbar.js
@@ -6,3 +6,7 @@ function dreibalkensymbol() {
   x.className = "topnav";
   }
 }
+window.onload = () => {
+    document.getElementsByClassName("dropdown-content")[0].innerHTML = '<a href="/content/schule/timetable/timetable.html" target="_blank">Stundenplan 2023/2024</a>'+
+    '<a href="https://youtube-crew.netlify.app" target="_blank">YouTube- Team</a>';
+}
