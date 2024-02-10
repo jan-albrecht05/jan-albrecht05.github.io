59444ec5d0877448d8c0003016b497f1bb3ceeac
Jan <janalbrecht05@gmail.com>
2024-02-11 00:23:19 +0100

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -7,6 +7,5 @@ function dreibalkensymbol() {
   }
 }
 window.onload = () => {
-    document.getElementsByClassName("dropdown-content")[0].innerHTML = '<a href="/content/schule/timetable/timetable.html" target="_blank">Stundenplan 2023/2024</a>'+
-    '<a href="https://youtube-crew.netlify.app" target="_blank">YouTube- Team</a>';
+    document.getElementsByClassName("dropdown-content")[0].innerHTML = '<a href="/content/schule/timetable/timetable.html" target="_blank">Stundenplan 2023/2024</a>'+'<a href="https://youtube-crew.netlify.app" target="_blank">YouTube- Team</a>'+'<a href="https://youtube.com/@Homburgschule" target="_blank">PvH YouTube- Kanal</a>';
 }
