9b57f59b10dae5818f45e2d90957a65cbb7ee3a9
Jan <janalbrecht05@gmail.com>
2024-06-12 13:23:02 +0200

    Update mode.js

--- js/mode.js
+++ js/mode.js
@@ -1,6 +1,6 @@
 const getChk = document.querySelector('.theme-input input[type="checkbox"]');
 const savedTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
-cookiesok = localStorage.getItem("cookies-accepted") ? localStorage.getItem('theme') : false;
+cookiesok = localStorage.getItem("cookies-accepted") ? localStorage.getItem('cookies-accepted') : false;
 
 if (savedTheme && cookiesok) {
     document.documentElement.setAttribute('data-theme', savedTheme);
