694d138889722ce50e4066557310b3051f834007
Jan <78158642+jan-albrecht05@users.noreply.github.com>
2024-10-01 13:30:25 +0200

    adding parts of the dimentions page

---
+++ content/tools/aspect-ratio/script.js
@@ -0,0 +1,18 @@
+function onetoone(){
+    document.getElementById("output").style.aspectRatio = "1/1";
+}
+function twotoone(){
+    document.getElementById("output").style.aspectRatio = "2/1";
+}
+function threetotwo(){
+    document.getElementById("output").style.aspectRatio = "3/2";
+}
+function fourtothree(){
+    document.getElementById("output").style.aspectRatio = "4/3";
+}
+function sixteentonine(){
+    document.getElementById("output").style.aspectRatio = "16/9";
+}
+function sixteentoten(){
+    document.getElementById("output").style.aspectRatio = "16/10";
+}
